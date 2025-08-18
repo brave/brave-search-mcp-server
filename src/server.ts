81cfe6df5725d69868f70cfdad651aaf97b53bfd
Sampson <jjdsampson@gmail.com>
2025-08-18 10:26:21 -0500

    use correct version number

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import tools from './tools/index.js';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { SetLevelRequestSchema } from '@modelcontextprotocol/sdk/types.js';
+import pkg from '../package.json' with { type: 'json' };
 
 export default function createMcpServer(): McpServer {
   const mcpServer = new McpServer(
     {
-      version: '0.1.0',
+      version: pkg.version,
       name: 'brave-search-mcp-server',
       title: 'Brave Search MCP Server',
     },
